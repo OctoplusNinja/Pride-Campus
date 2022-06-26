52b7a7c620a5a20fcefc31b3744a256b7f9440ef
lostgirljourney <falgunisarkar526@gmail.com>
2022-06-26 05:57:58 +0530

    minor fixes 🙂

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,24 +1,23 @@
 const fs = require('fs');
 
 module.exports = {
-	name: 'interactionCreate',
+  name: 'interactionCreate',
 
-	async execute(interaction, client) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-		if (!interaction.isCommand()) return;
+  async execute(interaction, client) {
+    if (!interaction.isCommand()) return;
 
-		const command = client.commands.get(interaction.commandName);
+    const command = client.commands.get(interaction.commandName);
 
-		if (!command) return;
+    if (!command) return;
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			await interaction.reply({
-				content: 'There was an error trying to execute that command!',
-				ephemeral: true
-			});
-		}
-	}
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: 'There was an error trying to execute that command!',
+        ephemeral: true
+      });
+    }
+  }
 };
